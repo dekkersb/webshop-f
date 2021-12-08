fee9bd1b39dade8c833c7a401a26ba4f4a1be133
Bart Dekkers <bart@recranet.com>
2021-12-08 14:02:25 +0100

    Herstel

---
+++ App/src/app/product-list/product-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Product} from "./product.model";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [
+  new Product('Tandpasta', 2.12)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
