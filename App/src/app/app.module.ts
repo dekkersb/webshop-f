99da6eb337a2b4d5b9288c9fbf0d26c90fc51449
Bart Dekkers <bart@recranet.com>
2021-12-14 16:40:03 +0100

    Added Routing

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -7,6 +7,17 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductItemComponent } from './product-list/product-item/product-item.component';
 import { CheckoutComponent } from './checkout/checkout.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'checkout', component: CheckoutComponent },
+];
 
 @NgModule({
   declarations: [
@@ -15,10 +26,13 @@ import { CheckoutComponent } from './checkout/checkout.component';
     ShoppingCartComponent,
     ProductListComponent,
     ProductItemComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    HomeComponent,
+    ContactComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
